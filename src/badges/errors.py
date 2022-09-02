d619c3b44d9b647bce2c8ecca7a575d42da421ef
Jan Wille <mail@janwille.de>
2022-09-02 23:46:51 +0200

    errors: remove redundant `pass`

--- src/badges/errors.py
+++ src/badges/errors.py
@@ -10,28 +10,18 @@ class BadgesBaseError(Exception):
 class ConnectionError(BadgesBaseError, requests.exceptions.ConnectionError):
     """it was not possible to connect to an external service"""
 
-    pass
-
 
 class ConnectionTimeoutError(BadgesBaseError, requests.exceptions.Timeout):
     """a request to an external service timed out"""
-
-    pass
 
 
 class ServerError(BadgesBaseError):
     """the server of an external service returned an error"""
 
-    pass
-
 
 class ParameterError(BadgesBaseError, TypeError, ValueError):
     """missconfiguration when calling a badges-function"""
 
-    pass
-
 
 class BadgeCreationError(BadgesBaseError):
     """it was not possible to create the requested badge"""
-
-    pass
